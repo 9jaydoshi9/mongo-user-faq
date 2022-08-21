276bd93937ae423e6513fd8520cb18aa0120bcae
Jay <jaydoshi75@gmail.com>
2022-08-21 17:11:05 +0530

    user: add welcome email for user.

--- util/util.js
+++ util/util.js
@@ -1,18 +1,35 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { transporter } = require("../config/email.config");
+
 const randomThreeDigits = () => (Math.random() + 1).toString(36).substring(2, 5);
 
 const generateHash = async (data) => bcrypt.hash(data, 10); // salt rounds
 
 const compareHash = async (password, hash) => bcrypt.compare(password, hash);
 
-const generateToken = (user) => jwt.sign(user, process.env.JWTSECRET || 'SECRET', {
-  expiresIn: '7d',
-});
+const generateToken = (user) =>
+  jwt.sign(user, process.env.JWTSECRET || "SECRET", {
+    expiresIn: "7d",
+  });
+
+const sendEmail = async (emailTo, emailData) => {
+  try {
+    await transporter.sendMail({
+      from: process.env.EMAIL_FROM || "support@abc.ok", // sender address
+      to: emailTo, // receiver email
+      subject: emailData.subject,
+      text: emailData.text,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   randomThreeDigits,
   generateHash,
   compareHash,
   generateToken,
+  sendEmail,
 };
